2fdee73b2bcffbeb2c49d03e68edc901b2b5542f
NikashDevX <lamsalnikash312@gmail.com>
2026-02-01 00:31:45 +0545

    auth

---
+++ User_Authentication/authenticateapp/views.py
@@ -0,0 +1,59 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate , login
+# Create your views here.
+
+
+def index(request):
+    return render(request,"index.html")
+
+def login_view(request):
+    if request.method == "POST":
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = User.objects.filter(username = username)
+            if not user.exists():
+                messages.success(request,"user not found")
+                return redirect('/login/')
+            user = authenticate(username = username , password = password)
+            print(user)
+            if not user:
+                messages.success(request , "Incorrect password")
+
+                return redirect('/login/')
+            login(request , user)
+            return redirect('/')
+
+    return render(request,"login.html" )
+
+def register_view(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+
+
+        user = User.objects.filter(username = username)
+        if user.exists():
+           messages.success(request,"username already taken")
+           return redirect('/register/')
+        
+        user = User.objects.create(
+            username = username,
+            first_name = first_name,
+            last_name = last_name
+        )
+        user.set_password(password)
+        user.save()
+        messages.success(request,"Account Created")
+        return redirect('/register/')
+
+        
+
+
+
+
+    return render(request,"register.html" )
+
