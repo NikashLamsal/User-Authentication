f86cfbf1832bb64f4edc0aacd1cd9b8a7a6520f3
NikashDevX <lamsalnikash312@gmail.com>
2026-02-05 19:31:28 +0545

    all auth

--- All_Auth/All_Auth/settings.py
+++ All_Auth/All_Auth/settings.py
@@ -31,14 +31,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
+
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'allauth',
+
+
 ]
+
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -48,24 +50,30 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
 
-ROOT_URLCONF = 'All_Auth.urls'
+
+
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.request',
+
             ],
         },
     },
 ]
+
+
 
 WSGI_APPLICATION = 'All_Auth.wsgi.application'
 
--- All_Auth/allauthapp/admin.py
+++ All_Auth/allauthapp/admin.py
@@ -1,3 +1,3 @@
 from django.contrib import admin
-
+from allauthapp import models 
 # Register your models here.
